6e2b3e8bcd35de1e495ce7fcc573b7f92e47039d
Giovanni <fruci.giovi@gmail.com>
2024-04-22 16:48:53 +0200

    finish

---
+++ src/ButtonComponent.jsx
@@ -0,0 +1,17 @@
+ import React from "react"
+ 
+ 
+ function ButtonComponent(propsObj) {
+    
+return (
+    <div>
+<button>{propsObj.content}</button>
+
+
+    </div>
+);
+
+
+ } 
+
+ export default ButtonComponent
---
+++ src/ClassComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class ClassComponent extends React.Component {
+    render(){
+        return <div>
+            <img src="https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg"/>
+        </div>
+    }
+}
+
+export default ClassComponent
